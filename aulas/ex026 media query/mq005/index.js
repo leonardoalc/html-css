043d881b6a90727ca41a9be207249b11c1935f56
LeL-nardo <sandroleonardoads@gmail.com>
2022-11-23 11:31:16 -0300

    media queries  + javascript resize

--- aulas/ex026 media query/mq005/index.js
+++ aulas/ex026 media query/mq005/index.js
@@ -1,5 +1,14 @@
 const itens = document.getElementById("itens")
 let menuOpn = false
+const corpo = window.document.body
+const corpo_observer = new ResizeObserver((entries) => {
+    if (window.innerWidth >= 768) {
+        itens.style.display = "block"
+    } else {
+        itens.style.display = "none"
+    }
+})
+corpo_observer.observe(corpo)
 burguer.addEventListener("click", () => {
     if (!menuOpn) {
         itens.setAttribute("class", "aberto")
